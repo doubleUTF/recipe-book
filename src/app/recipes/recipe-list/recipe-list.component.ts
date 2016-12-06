d63c386de49632ef84df91a7ac363d5f536a24f9
BigD <dlau373@gmail.com>
2016-12-06 09:03:16 -0800

    Added recipe detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe';
 
 @Component({
@@ -7,10 +7,14 @@ import {Recipe} from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
   recipes:Recipe[]=[];
+  @Output() recipeSelected= new EventEmitter<Recipe>();
   dummy_recipe = new Recipe('Dummy','Dummy','../../assets/elaicheesecake.png');
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelected(recipe:Recipe){
+    this.recipeSelected.emit(recipe);
+  }
 }
